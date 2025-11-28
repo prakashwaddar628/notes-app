d41e9efed56952444046e65759a8b9732fa38fc5
prakashwaddar628 <prakashwaddar628@gmail.com>
2025-11-28 11:55:44 +0530

    notes module impleneted for creating the notes table

---
+++ backend/app/models/note.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey, func
+from sqlalchemy.orm import relationship
+from app.db.base_class import Base
+
+class Note(Base):
+    __tablename__ = "notes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    title = Column(String(255), index=True)
+    content = Column(Text, nullable=True)
+
+    version = Column(Integer, default=1, nullable=False)
+
+    is_archived = Column(Integer, default=0, nullable=False)
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(
+        DateTime(timezone=True), 
+        server_default=func.now(), 
+        onupdate=func.now()
+        )
+    
+
+    user = relationship("User", back_populates="notes")

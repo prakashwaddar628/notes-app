23eed1cad9a392e11a744269a65503c81340205b
prakashwaddar628 <prakashwaddar628@gmail.com>
2025-11-28 14:07:06 +0530

    added router api in main

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
+
 from app.db.session import Base, engine
+from app.api.v1.auth import router as auth_router
 
 app = FastAPI(title="Notes Sync API")
 
 @app.on_event("startup")
 def on_startup():
     Base.metadata.create_all(bind=engine)
+
+app.include_router(auth_router)
 
 @app.get("/health")
 def health_check():

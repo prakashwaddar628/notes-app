a74fa23ed52a378772867a9c8d4575c04be4b82b
prakashwaddar628 <prakashwaddar628@gmail.com>
2025-11-28 11:57:17 +0530

    updated the main file

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from app.db.session import Base, engine
 
 app = FastAPI(title="Notes Sync API")
+
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
 
 @app.get("/health")
 def health_check():

d323d7a4ba2380715e7e1f0d0b01f7c6362bb367
prakashwaddar628 <prakashwaddar628@gmail.com>
2025-11-29 11:36:32 +0530

    Integrate Middleware in main.py

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,8 @@ from app.db.session import Base, engine
 from app.api.v1.auth import router as auth_router
 from app.api.v1.notes import router as notes_router
 
+from app.middleware.rate_limit import RateLimitMiddleware
+
 app = FastAPI(title="Notes Sync API")
 
 @app.on_event("startup")
@@ -12,6 +14,8 @@ def on_startup():
 
 app.include_router(auth_router)
 app.include_router(notes_router)
+
+app.add_middleware(RateLimitMiddleware)
 
 @app.get("/health")
 def health_check():

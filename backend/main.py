e00179dfd61cd825661b18150292c6fdac735154
prakashwaddar628 <prakashwaddar628@gmail.com>
2025-11-30 10:10:43 +0530

    cors added for the final stage of backend

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.db.session import Base, engine
 from app.api.v1.auth import router as auth_router
@@ -7,6 +8,20 @@ from app.api.v1.notes import router as notes_router
 from app.middleware.rate_limit import RateLimitMiddleware
 
 app = FastAPI(title="Notes Sync API")
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 def on_startup():
